8563740f47a9dbd8be0161382e0ab59a77e5d891
B-Bel88 <90757682+B-Bel88@users.noreply.github.com>
2021-10-12 16:31:52 +0200

    Add files via upload

---
+++ Opdracht3_basic_js/script.js
@@ -0,0 +1,26 @@
+var age= 28;
+
+    if (age == 18){
+        console.log('You are 18 so you may enter');
+    } 
+    if (age > 18){
+        console.log('You are over 18 you may enter');
+    }   
+    if (age < 18){
+        console.log('You are to joung take a hike');
+    }
+var isFemale = false;
+
+    if (isFemale == true){
+        console.log('You are welcome to the ladies night');
+    }
+    if (isFemale == false){
+        console.log('You got it wrong you have Dong BYE BYE');
+    }
+var  driverStatus = "bob";
+    if (driverStatus == "bob"){
+        console.log('Thank you for taking one for the team you may drive');
+    }
+    else {
+        console.log('You iz drunk girlfriend call an UBER!');
+    }
