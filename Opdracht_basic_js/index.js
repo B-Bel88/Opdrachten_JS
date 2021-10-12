3491ce0979de6f1d8e6363b16eb9017515c5004b
B-Bel88 <90757682+B-Bel88@users.noreply.github.com>
2021-10-12 11:51:54 +0200

    Add files via upload

---
+++ Opdracht_basic_js/index.js
@@ -0,0 +1,18 @@
+console.log("Hello Winc Academy")
+
+let brad= 'Brad';
+console.log(brad);
+let numbers= 4+4;
+console.log(numbers);
+let addition= 15+15;
+console.log(addition)
+let subtract= 30-10;
+console.log(subtract)
+let multyply= 2*15;
+console.log(multyply);
+let devision= 30/2;
+console.log(devision)
+let modulus= 10 % 100;
+console.log(modulus);
+let age= '33';
+console.log(age) 
