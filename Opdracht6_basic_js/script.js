57f679d2cf1e746c8924ed2244d9561b38ba8984
B-Bel88 <90757682+B-Bel88@users.noreply.github.com>
2021-10-14 14:19:03 +0200

    Add files via upload

---
+++ Opdracht6_basic_js/script.js
@@ -0,0 +1,22 @@
+/* making pizza*/
+const makingPizza = function(){
+    console.log('Make the pizzadough');
+    console.log('Prep toppings');
+    console.log('making tomato sauce');
+    console.log('combining ingrediants');
+    console.log('cook in woodfire Pizzaoven');
+};
+/* making sushi*/
+const makingSushi = function(){
+    console.log('cook sushi rice');
+    console.log('prep ingediants');
+    console.log('prep sushi rolling mat');
+    console.log('combine ingrediants');
+    console.log('roll into nice roll');
+};
+
+makingPizza();
+makingPizza();
+makingSushi();
+makingSushi();
+makingSushi();
