24040d6ffb74a6c1a741fee8fc74d00d9faec8bd
B-Bel88 <90757682+B-Bel88@users.noreply.github.com>
2021-10-15 10:49:22 +0200

    Add files via upload

---
+++ Opdracht7_basic_js/script.js
@@ -0,0 +1,10 @@
+const painting = function (wallDirection, paintcolors){
+    console.log ("the " + wallDirection + " wall has been painted " + paintcolors );       
+
+};
+
+const paintcolors = "green";
+const wallDirection = 'north';
+
+
+painting(wallDirection, paintcolors);
